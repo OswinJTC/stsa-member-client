3c18e17fcd6dacd2d20b7cabf89f448df98215f2
Oswin <129711775+OswinJTC@users.noreply.github.com>
2024-08-21 20:35:39 +0800

    Update axiosConfig.js

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://stsa-member-server.onrender.com',
+    baseURL: 'https://member-server.stsa.tw',
     headers: {"skip-browser-warning": "true"}
 });
